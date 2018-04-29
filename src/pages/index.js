167db2f93737c0a93cb0bba8e835b7afe9dd2e15
Marcel Fahle <m.fahle@gmail.com>
2018-04-29 17:33:07 +0200

    I changesd stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <h1>Hi Tommy</h1>
+    <p>More HTML</p>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
